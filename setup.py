9525f176b319b9d1f976522c53650c34d8fbda34
Keita Mori <keita011@gmail.com>
2020-10-06 00:01:31 +0900

    Modify packages

--- setup.py
+++ setup.py
@@ -4,9 +4,10 @@ setup(
     name='openapi-redoc-schema-def',
     version='0.1.0',
     install_requres=['PyYAML'],
+    packages=["openapi-redoc-schema-def"],
     entry_points={
         'console_scripts': [
-            "openapi-redoc-schema-def=main:main"
+            "openapi-redoc-schema-def=openapi-redoc-schema-def.main:main"
         ]
     }
 )
